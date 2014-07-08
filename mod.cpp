4fb8acefc8d506c491b40b99d2bc5f024a8d32b7
oscarmolinadev <oscarmolinadev@gmail.com>
2014-07-08 21:17:07 +0200

    First commit

---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "Estandard Grup Cavallers del Cel v1.9";
+picture = "logo_ccescut_ca.paa";
+actionName = "Portal web";
+action = "http://www.cavallersdelcel.cat";
+description = "Estàndard d'addons dels grup de l'ArmA de la Comunitat Catalana de Simulació, Cavallers del Cel";
